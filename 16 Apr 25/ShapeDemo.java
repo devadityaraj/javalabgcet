a347f0855458bed839a04af157a7ecfd2fca52bb
Aditya Raj <adityaraj94505@gmail.com>
2025-04-16 12:13:27 +0530

    update

--- 16 Apr 25/ShapeDemo.java
+++ 16 Apr 25/ShapeDemo.java
@@ -24,6 +24,22 @@ class Circle extends Shape {
     }
 }
 
+class Square extends Shape {
+    private double length;
+
+    Square(double length) {
+        this.length = length;
+    }
+
+    public void draw() {
+        System.out.println("Drawing a Square");
+    }
+
+    public double area() {
+        return length * length;
+    }
+}
+
 class Rectangle extends Shape {
     private double length;
     private double width;
@@ -46,9 +62,12 @@ public class ShapeDemo {
     public static void main(String[] args) {
         Shape s1 = new Circle(10);
         Shape s2 = new Rectangle(5, 10);
+        Shape s3 = new Square(4);
         s1.draw();
         System.out.println("Area of circle: " + s1.area());
         s2.draw();
         System.out.println("Area of rectangle: " + s2.area());
+        s3.draw();
+        System.out.println("Area of square: " + s3.area());
     }
 }
